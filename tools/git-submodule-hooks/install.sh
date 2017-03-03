77b45a13434624c49edf98b614af30b86888319f
links234 <murtaza_alexandru73@yahoo.com>
2017-03-03 11:29:50 -0800

    Fix git submodule hooks

--- tools/git-submodule-hooks/install.sh
+++ tools/git-submodule-hooks/install.sh
@@ -11,4 +11,13 @@ it.
 $location
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-cp -r ./hooks $location
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+cp -r $MY_PATH/hooks $location
+
